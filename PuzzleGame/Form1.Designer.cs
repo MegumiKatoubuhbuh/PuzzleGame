176caae3d2a44b6f88655f9bcd43fbe4e4eb99be
Megumi Katou <138238569+MegumiKatoubuhbuh@users.noreply.github.com>
2023-12-17 12:40:19 +0700

    first?

---
+++ PuzzleGame/Form1.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace PuzzleGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxOriginal = new System.Windows.Forms.PictureBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOriginal)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBox9);
+            this.groupBox1.Controls.Add(this.pictureBox8);
+            this.groupBox1.Controls.Add(this.pictureBox7);
+            this.groupBox1.Controls.Add(this.pictureBox6);
+            this.groupBox1.Controls.Add(this.pictureBox5);
+            this.groupBox1.Controls.Add(this.pictureBox4);
+            this.groupBox1.Controls.Add(this.pictureBox3);
+            this.groupBox1.Controls.Add(this.pictureBox2);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(565, 581);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "PuzzleBox";
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.Image = global::PuzzleGame.Properties.Resources._8;
+            this.pictureBox9.Location = new System.Drawing.Point(378, 393);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(180, 180);
+            this.pictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox9.TabIndex = 8;
+            this.pictureBox9.TabStop = false;
+            this.pictureBox9.Tag = "8";
+            this.pictureBox9.Click += new System.EventHandler(this.pictureBox9_Click);
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Image = global::PuzzleGame.Properties.Resources._7;
+            this.pictureBox8.Location = new System.Drawing.Point(192, 393);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(180, 180);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox8.TabIndex = 7;
+            this.pictureBox8.TabStop = false;
+            this.pictureBox8.Tag = "7";
+            this.pictureBox8.Click += new System.EventHandler(this.pictureBox8_Click);
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Image = global::PuzzleGame.Properties.Resources._6;
+            this.pictureBox7.Location = new System.Drawing.Point(6, 393);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(180, 180);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox7.TabIndex = 6;
+            this.pictureBox7.TabStop = false;
+            this.pictureBox7.Tag = "6";
+            this.pictureBox7.Click += new System.EventHandler(this.pictureBox7_Click);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::PuzzleGame.Properties.Resources._5;
+            this.pictureBox6.Location = new System.Drawing.Point(378, 207);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(180, 180);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Tag = "5";
+            this.pictureBox6.Click += new System.EventHandler(this.pictureBox6_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::PuzzleGame.Properties.Resources._4;
+            this.pictureBox5.Location = new System.Drawing.Point(192, 207);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(180, 180);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Tag = "4";
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::PuzzleGame.Properties.Resources._3;
+            this.pictureBox4.Location = new System.Drawing.Point(6, 207);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(180, 180);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Tag = "3";
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::PuzzleGame.Properties.Resources._2;
+            this.pictureBox3.Location = new System.Drawing.Point(378, 21);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(180, 180);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Tag = "2";
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::PuzzleGame.Properties.Resources._1;
+            this.pictureBox2.Location = new System.Drawing.Point(192, 21);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(180, 180);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Tag = "1";
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::PuzzleGame.Properties.Resources._0;
+            this.pictureBox1.Location = new System.Drawing.Point(6, 21);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(180, 180);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Tag = "0";
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBoxOriginal
+            // 
+            this.pictureBoxOriginal.Image = global::PuzzleGame.Properties.Resources.original;
+            this.pictureBoxOriginal.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBoxOriginal.InitialImage")));
+            this.pictureBoxOriginal.Location = new System.Drawing.Point(18, 21);
+            this.pictureBoxOriginal.Name = "pictureBoxOriginal";
+            this.pictureBoxOriginal.Size = new System.Drawing.Size(300, 300);
+            this.pictureBoxOriginal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxOriginal.TabIndex = 1;
+            this.pictureBoxOriginal.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.pictureBoxOriginal);
+            this.groupBox2.Location = new System.Drawing.Point(594, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(334, 339);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Original";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(724, 522);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 38);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Chơi";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(607, 374);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 25);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Time : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(776, 374);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 25);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(607, 442);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(136, 25);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Moves made: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(776, 442);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 25);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.ClientSize = new System.Drawing.Size(995, 614);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOriginal)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBoxOriginal;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ PuzzleGame/Form1.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PuzzleGame
+{
+    public partial class Form1 : Form
+    {
+        static bool isPlaying;              //thong tin co dang choi khong
+        static int movesMade;               //so lan di chuyen
+        static PictureBox emptyPictureBox;  //tham chieu pictureBox dang trong
+        static int emptyIndex;
+        static List<Bitmap> images = new List<Bitmap>();    //danh sach cac anh
+        static TimeSpan gameTime;     //thoi gian tu luc bat dau
+
+        public Form1()
+        {
+            InitializeComponent();
+            images.AddRange(new Bitmap[]{ Properties.Resources._0,
+                                        Properties.Resources._1,Properties.Resources._2,
+                                        Properties.Resources._3,Properties.Resources._4,
+                                        Properties.Resources._5,Properties.Resources._6,
+                                        Properties.Resources._7});
+            emptyIndex = 8;
+            movesMade = 0;
+            isPlaying = false;
+
+            groupBox1.Controls.Add(pictureBox1);
+            groupBox1.Controls.Add(pictureBox2);
+            groupBox1.Controls.Add(pictureBox3);
+            groupBox1.Controls.Add(pictureBox4);
+            groupBox1.Controls.Add(pictureBox5);
+            groupBox1.Controls.Add(pictureBox6);
+            groupBox1.Controls.Add(pictureBox7);
+            groupBox1.Controls.Add(pictureBox8);
+            groupBox1.Controls.Add(pictureBox9);
+        }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!isPlaying)                     //neu dang khong choi
+            {
+                emptyPictureBox = pictureBox9;
+                Shuffle(images);
+                movesMade = 0;
+                gameTime = new TimeSpan(0, 0, 0);
+                timer1.Start();
+                isPlaying = true;
+            }
+            else                                   //neu dang trong qua trinh choi
+            {
+                if(MessageBox.Show("Ban chua hoan thanh, ban co muon choi lai khong","Thong bao",MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    emptyPictureBox = pictureBox9;
+                    Shuffle(images);
+                    movesMade = 0;
+                    label4.Text = movesMade.ToString();
+                    gameTime = new TimeSpan(0, 0, 0);
+                    timer1.Start();
+                }
+            }
+        }
+
+        
+
+        private void pictureBoxClick(int index)
+        {
+
+        }
+
+        
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            PictureBox selectPictureBox = sender as PictureBox;
+            int index = Convert.ToInt32(selectPictureBox.Tag);
+
+            if (IsAdj(selectPictureBox))
+            {
+                swapImage(emptyPictureBox, selectPictureBox);
+                movesMade++;
+                label4.Text = movesMade.ToString();
+            }
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            PictureBox selectPictureBox = sender as PictureBox;
+            int index = Convert.ToInt32(selectPictureBox.Tag);
+
+            if (IsAdj(selectPictureBox))
+            {
+                swapImage(emptyPictureBox, selectPictureBox);
+                movesMade++;
+                label4.Text = movesMade.ToString();
+            }
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            PictureBox selectPictureBox = sender as PictureBox;
+            int index = Convert.ToInt32(selectPictureBox.Tag);
+
+            if (IsAdj(selectPictureBox))
+            {
+                swapImage(emptyPictureBox, selectPictureBox);
+                movesMade++;
+                label4.Text = movesMade.ToString();
+            }
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            PictureBox selectPictureBox = sender as PictureBox;
+            int index = Convert.ToInt32(selectPictureBox.Tag);
+
+            if (IsAdj(selectPictureBox))
+            {
+                swapImage(emptyPictureBox, selectPictureBox);
+                movesMade++;
+                label4.Text = movesMade.ToString();
+            }
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            PictureBox selectPictureBox = sender as PictureBox;
+            int index = Convert.ToInt32(selectPictureBox.Tag);
+
+            if (IsAdj(selectPictureBox))
+            {
+                swapImage(emptyPictureBox, selectPictureBox);
+                movesMade++;
+                label4.Text = movesMade.ToString();
+            }
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+            PictureBox selectPictureBox = sender as PictureBox;
+            int index = Convert.ToInt32(selectPictureBox.Tag);
+
+            if (IsAdj(selectPictureBox))
+            {
+                swapImage(emptyPictureBox, selectPictureBox);
+                movesMade++;
+                label4.Text = movesMade.ToString();
+            }
+        }
+
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+            PictureBox selectPictureBox = sender as PictureBox;
+            int index = Convert.ToInt32(selectPictureBox.Tag);
+
+            if (IsAdj(selectPictureBox))
+            {
+                swapImage(emptyPictureBox, selectPictureBox);
+                movesMade++;
+                label4.Text = movesMade.ToString();
+            }
+        }
+
+        private void pictureBox8_Click(object sender, EventArgs e)
+        {
+            PictureBox selectPictureBox = sender as PictureBox;
+            int index = Convert.ToInt32(selectPictureBox.Tag);
+
+            if (IsAdj(selectPictureBox))
+            {
+                swapImage(emptyPictureBox, selectPictureBox);
+                movesMade++;
+                label4.Text = movesMade.ToString();
+            }
+        }
+
+        private void pictureBox9_Click(object sender, EventArgs e)
+        {
+            PictureBox selectPictureBox = sender as PictureBox;
+            int index = Convert.ToInt32(selectPictureBox.Tag);
+
+            if (IsAdj(selectPictureBox))
+            {
+                swapImage(emptyPictureBox, selectPictureBox);
+                movesMade++;
+                label4.Text = movesMade.ToString();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            gameTime = gameTime.Add(new TimeSpan(0, 0, 1));
+            label2.Text = gameTime.ToString(@"hh\:mm\:ss");
+            if(isAllDone())
+            {
+                string thongbao = "Chuc mung ban da hoan thanh tro choi ";
+                string diem = "\nSo nuoc di: " + movesMade.ToString();
+                string thoiGian = "\nThoi gian hoan thanh: " + gameTime.ToString();
+                MessageBox.Show(thongbao+diem+thoiGian,"Thong bao");
+            }
+
+        }
+
+        public void Shuffle(List<Bitmap> images)
+        {
+            //thay doi thu tu lai nhu ban dau
+            for(int i=0;i<8;i++)
+            {
+                (groupBox1.Controls[i] as PictureBox).Image = images[i];
+            }
+            pictureBox9.Image = null;
+            //thuc hien xao tron anh 
+            Random random = new Random();
+            for(int i = 0;i<500;i++)
+            {
+                PictureBox randomPictureBox = groupBox1.Controls[random.Next(9)] as PictureBox;
+                if (IsAdj(randomPictureBox))
+                {
+                    swapImage(emptyPictureBox, randomPictureBox);
+                }
+            }
+        }
+
+        //kiem tra co ke voi o trong khong
+        private static bool IsAdj(PictureBox selectPictureBox)
+        {
+            int index = Convert.ToInt32(selectPictureBox.Tag);
+            int emptyIndex = Convert.ToInt32(emptyPictureBox.Tag);
+            int emptyX = emptyIndex / 3;
+            int emptyY = emptyIndex % 3;
+
+            int x = index / 3;
+            int y = index % 3;
+
+            if (x == emptyX && (y == emptyY - 1 || y == emptyY + 1))
+                return true;
+
+            if (y == emptyY && (x == emptyX - 1 || x == emptyX + 1))
+                return true;
+
+            return false;
+        }
+
+        private void swapImage(PictureBox pictureBoxEmpty, PictureBox selectPictureBox)
+        {
+            pictureBoxEmpty.Image = selectPictureBox.Image;
+            selectPictureBox.Image = null;
+            emptyPictureBox = selectPictureBox;
+
+        }
+
+        private bool isAllDone()
+        {
+            int i = 0;
+            for(i = 0; i < 8; i++)
+            {
+                if ((groupBox1.Controls[i] as PictureBox).Image != images[i])
+                    break;
+            }
+            if (i == 8)
+            {
+                timer1.Stop();
+                isPlaying = false;
+                return true;
+            }
+            else
+                return false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
